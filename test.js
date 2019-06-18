992b03695acef7af3ae7ba7179fa8f5f8965b1a3
Fela Maslen <felamaslen@gmail.com>
2019-06-18 11:25:44 +0100

    Use mocha instead of ava for testing (for backwards compatibility with node 6)

--- test.js
+++ test.js
@@ -1,23 +1,25 @@
-const test = require('ava');
+const assert = require('assert');
 
 const stringDiff = require('./index');
 
-test('stringDiff outputs a string diff (using a stream)', (t) => {
-  let data = '';
-  const stream = {
-    write: chunk => {
-      data += chunk;
-    }
-  };
+describe('stringDiff', () => {
+  it('outputs a string diff (using a stream)', () => {
+    let data = '';
+    const stream = {
+      write: chunk => {
+        data += chunk;
+      }
+    };
 
-  const result = stringDiff('foo1', 'foo2', stream);
+    const result = stringDiff('foo1', 'foo2', stream);
 
-  t.is(data, '\x1b[0m\x1b[2mfoo\x1b[0m\x1b[31m1\x1b[0m\x1b[32m2');
-  t.is(result, null);
-});
+    assert.equal(data, '\x1b[0m\x1b[2mfoo\x1b[0m\x1b[31m1\x1b[0m\x1b[32m2');
+    assert.equal(result, null);
+  });
 
-test('stringDiff outputs a string diff (without using a stream)', (t) => {
-  const data = stringDiff('foo1', 'foo2');
+  it('outputs a string diff (without using a stream)', () => {
+    const data = stringDiff('foo1', 'foo2');
 
-  t.is(data, '\x1b[0m\x1b[2mfoo\x1b[0m\x1b[31m1\x1b[0m\x1b[32m2');
+    assert.equal(data, '\x1b[0m\x1b[2mfoo\x1b[0m\x1b[31m1\x1b[0m\x1b[32m2');
+  });
 });
